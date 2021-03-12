b7e7ca07fb82ac6baff1cefee7e37d073c840d87
Nikhil Sriram <Nikhil.Sriram5@gmail.com>
2021-03-12 09:55:16 -0800

    comment additions and deletions now possible on posts

---
+++ src/CommentsComponent.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useParams, useLocation, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {v4 as uuidv4} from 'uuid';
+
+function CommentComponent({id,comments,setComments}){
+    const dispatch = useDispatch();
+    const blankForm = {comment:""};
+    const [formData,setFormData] = useState(blankForm);
+    
+    const handleChange = (evt)=>{
+        const {name,value} = evt.target;
+        setFormData(data=>({
+            ...data,[name]:value
+        }));
+    }
+    const handleSubmit = (evt)=>{
+        evt.preventDefault();
+        const commentId = uuidv4();
+        dispatch({type:"ADD_COMMENT",id,commentId:uuidv4(),comment:formData})
+        setComments({[commentId]:{comment:formData.comment},...comments});
+        setFormData(blankForm);
+    }
+    const handleDelete = (key)=>{
+        dispatch({type:"DELETE_COMMENT",id,key})
+        const {[key]:a,...rest} = comments;
+        setComments(rest);
+    }
+    return(
+        <div>
+            <h3>Comments</h3>
+            {
+                Object.keys(comments).map((key)=>{
+                    if (comments[key]){
+                        return(
+                            <div key={key}>
+                                <span onClick={()=>handleDelete(key)}>x</span><span>{`     ${comments[key].comment}`}</span>
+                            </div>);
+                    }
+                    return null;
+                })
+
+            }
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input type="text" className="form-control" name="comment" value={formData["comment"]} onChange={handleChange} autoComplete="comment"/>
+                    <button type="submit" className="btn btn-primary float-right">Add</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default CommentComponent;
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -5,20 +5,52 @@ const INITIAL_STATE = { posts: {}, error: false, comments: {} };
 function rootReducer(state = INITIAL_STATE, action) {
     const post = action.post
     let key = action.id
+    let comments
     switch (action.type) {
 
         case 'ADD':
             key = uuidv4();
-            return { posts: {...state.posts, [key]: post }, error: false, comments: state.comments };
+            return {
+                posts: {...state.posts, [key]: post },
+                error: false,
+                comments: {
+                    [key]: {},
+                    ...state.comments
+                }
+            };
         case 'EDIT':
             return { posts: {...state.posts, [key]: post }, error: false, comments: state.comments };
         case 'REMOVE':
-            const {
+            let {
                 [key]: a, ...rest
             } = state.posts
             return { posts: rest, error: false, comments: state.comments }
         case 'LOAD':
             return state;
+        case 'ADD_COMMENT':
+            comments = state.comments;
+            const existingComments = comments[action.id];
+            const newComments = {
+                [action.id]: {
+                    [action.commentId]: action.comment,
+                    ...existingComments
+                }
+            }
+            return { comments: newComments, posts: state.posts, error: false }
+
+        case 'DELETE_COMMENT':
+            comments = state.comments[action.id];
+            const {
+                [action.key]: b, ...commentsrest
+            } = comments;
+            return {
+                posts: state.posts,
+                error: false,
+                comments: {
+                    [action.id]: { commentsrest },
+                    ...state.comments
+                }
+            }
         default:
             return state;
     }
